92449be413904cdc470ca2d701f82c033dc665a0
SilverWingedSeraph <lfstindall@gmail.com>
2017-03-15 15:28:11 -0500

    Make FPS counter actually work. Oops.

--- src/fpscounter.rs
+++ src/fpscounter.rs
@@ -1,23 +1,34 @@
-use std::collections::VecDeque;
-
+use std::time::{Instant};
 pub struct FPSCounter {
-    queue: VecDeque<f64>,
+    frames_since_clear: u64,
+    supposed_time_since_clear: f64,
+    real_time_at_last_clear: Instant,
     fps: f64
 }
+
 impl FPSCounter {
     pub fn new() -> Self {
         FPSCounter {
-            queue: VecDeque::<f64>::with_capacity(120),
-            fps: 0.0
+            supposed_time_since_clear: 0.0,
+            frames_since_clear: 0,
+            real_time_at_last_clear: Instant::now(),
+            fps: 0.0,
         }
     }
+
     pub fn update(&mut self, dt: f64, sample_rate: f64) {
-        self.queue.push_back(dt);
-        let time_since_clear = self.queue.iter().fold(0.0, |a, i| a + i);
-        if time_since_clear >= sample_rate {
-            self.fps = self.queue.len() as f64 / time_since_clear;
-            self.queue = VecDeque::with_capacity(120);
+        self.supposed_time_since_clear += dt;
+        self.frames_since_clear += 1;
+
+        if self.supposed_time_since_clear >= sample_rate {
+            let elapsed = self.real_time_at_last_clear.elapsed();
+            let real_time_since_clear = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * (1.0/1000000000.0);
+            self.fps = self.frames_since_clear as f64 / real_time_since_clear;
+            self.supposed_time_since_clear = 0.0;
+            self.frames_since_clear = 0;
+            self.real_time_at_last_clear = Instant::now();
         }
     }
+
     pub fn get_fps(&self) -> f64 { self.fps }
 }
