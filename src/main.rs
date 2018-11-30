9ba37566456d71a5d8e9213230746daeeb9fd796
Drew Silcock <drew.silcock@stfc.ac.uk>
2018-11-30 00:01:51 +0000

    Fix OS X compile error by updating dependencies
    One of the dependencies relied on an old version of core-graphics
    (0.3.2) which no longer compiles on OS X.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate find_folder;
 extern crate update_rate;
 use piston_window::*;
 use text::Text;
-use update_rate::UpdateRateCounter;
+use update_rate::{RateCounter, RollingRateCounter};
 
 mod coloredrect;
 use coloredrect::ColoredRect;
@@ -13,7 +13,7 @@ fn get_glyphs(window: &PistonWindow) -> Glyphs {
         .for_folder("resources").unwrap();
     let ref font = assets.join("Anonymous.ttf");
     let factory = window.factory.clone();
-    return Glyphs::new(font, factory).unwrap()
+    return Glyphs::new(font, factory, TextureSettings::new()).unwrap()
 }
 
 fn main() {
@@ -27,48 +27,43 @@ fn main() {
     let mut glyphs = get_glyphs(&window);
 
     let mut window_size: (f64, f64) = (0.0, 0.0);
-    let mut fpscounter = UpdateRateCounter::new(60);
+    let mut fpscounter = RollingRateCounter::new(60);
     let fpsfont = Text::new(10);
 
-    while let Some(e) = window.next() {
-        match e {
-            Input::Render(r) => {
-                window_size = (r.width as f64, r.height as f64);
-                window.draw_2d(&e, |c, g| {
-                    clear([1.0; 4], g); // Clear to white
-                    rectangle(rect.color, // Color
-                              rect.position, // Position/size
-                              c.transform, g);
-                    fpsfont.draw(&format!("{:.0} FPS", fpscounter.rate()), 
-                        &mut glyphs, &c.draw_state,
-                        c.transform.trans(10.0, 12.0), // Set the position of the drawing
-                        g);
-                });
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            window_size = (r.width as f64, r.height as f64);
+            window.draw_2d(&e, |c, g| {
+                clear([1.0; 4], g); // Clear to white
+                rectangle(rect.color, // Color
+                          rect.position, // Position/size
+                          c.transform, g);
+                fpsfont.draw(&format!("{:.0} FPS", fpscounter.rate()), 
+                    &mut glyphs, &c.draw_state,
+                    c.transform.trans(10.0, 12.0), // Set the position of the drawing
+                    g).unwrap();
+            });
+        }
+
+        if let Some(u) = e.update_args() {
+            fpscounter.update();
+            rect.update(u.dt, window_size);
+        }
+
+        if let Some(Button::Keyboard(k)) = e.press_args() {
+            match k {
+                Key::W => {
+                    rect.change_velocity(1.1);
+                },
+                Key::S => {
+                    rect.change_velocity(0.9);
+                },
+                Key::F5 => {
+                    rect = ColoredRect::new();
+                },
+                _ => {}, // Catch all keys
             }
-            Input::Update(u) => {
-                fpscounter.update();
-                rect.update(u.dt, window_size);
-            }
-            Input::Press(b) => {
-                match b {
-                    Button::Keyboard(k) => {
-                        match k {
-                            Key::W => {
-                                rect.change_velocity(1.1);
-                            }
-                            Key::S => {
-                                rect.change_velocity(0.9);
-                            }
-                            Key::F5 => {
-                                rect = ColoredRect::new();
-                            }
-                            _ => {} // Catch all keys
-                        };
-                    }
-                    _ => {} // Catch non-keyboard buttons
-                };
-            }
-            _ => {} // Catch uninteresting events
         }
     }
 }
